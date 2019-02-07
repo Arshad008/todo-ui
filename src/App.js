4be284fe98c4f08f5afdf5a1175bdb0ab8a1d00d
arshad008 <arshadrauff008@gmail.com>
2019-02-07 13:43:53 +0530

    note side bar layout implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,101 @@
 import React, { Component } from 'react';
+import { 
+  Layout, 
+  Menu,
+  Icon,
+  Button,
+  Tooltip
+} from 'antd';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+const { 
+  Header, 
+  Content, 
+  Footer, 
+  Sider 
+} = Layout;
+
+const { SubMenu } = Menu;
+
 class App extends Component {
+  state = {
+    collapsed: false
+  }
+  onCollapse = (collapsed) => {
+    this.setState({collapsed});
+  }
   render() {
     return (
-      <div className="App">
-      </div>
+      // Main Layout
+      <Layout>
+        {/* Sider */}
+        <Sider
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
+          style={{ background: '#fff', alignContent: "center" }}
+          theme="light"
+        >
+          {/* Menu */}
+          <Menu            
+            defaultSelectedKeys={['1']}
+            mode="inline"
+          >
+            {/* Menu Items */}
+            <Menu.Item key="1" style={{marginTop: "50px"}}>
+              <Icon type="home"/>
+              <span>Home</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="database"/>
+              <span>My Todos</span>
+            </Menu.Item>
+            {/* Sub Menu */}
+            <SubMenu key="sub1" title={<span><Icon type="tags"/><span>Categories</span></span>}>
+              <Menu.Item key="3">
+                <Icon type="minus"/>
+                <span>Shopping</span>
+              </Menu.Item>
+              <Menu.Item key="4">
+                <Icon type="minus"/>
+                <span>Learning</span>
+              </Menu.Item>
+              <Menu.Item key="5">
+                <Icon type="minus"/>
+                <span>Evening activities</span>
+              </Menu.Item>
+            </SubMenu>
+            {/* Sub Menu End*/}
+            <Menu.Item key="6">
+              <Icon type="check"/>
+              <span>Finished</span>
+            </Menu.Item>            
+          </Menu> 
+          {/* Menu End */}
+          {/* Add New Button */}          
+          <div align="center" style={{marginTop: "20px", width: "100%"}}>
+          <Tooltip title="Add New Todo" placement="right">
+            <Button type="primary" icon="plus" shape="circle"></Button>
+          </Tooltip>            
+          </div>          
+        </Sider>
+        {/* Sider End */}
+        {/* Sub Layout */}
+        <Layout>
+          {/* Content */}
+          <Content>
+            <BrowserRouter>
+              <Switch>
+                {/* <Route path="/" component={Home} /> */}
+              </Switch>
+            </BrowserRouter>
+          </Content>
+          {/* Content End*/}          
+        </Layout>
+        {/* Sub layout end */}
+      </Layout>
+      // Main Layout End
     );
   }
 }
