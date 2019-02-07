ebf4219f26899b953fc3e8e96b87268dca9c7dbe
arshad008 <arshadrauff008@gmail.com>
2019-02-07 15:53:13 +0530

    todo list

---
+++ src/components/TodoList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import TodoListItem from './TodoListItem';
+import {
+    Row
+} from 'antd';
+
+const TodoList = (props) => {
+    let todos = props.todos.map((td, i)=>{
+        return <TodoListItem 
+                    key={i}
+                    todo={td}
+                    index={i}/>
+    });
+    return(
+        <Row gutter={16}>
+            {todos}
+        </Row>
+    );
+}
+
+export default TodoList;
