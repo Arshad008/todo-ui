fb8163c4258e65a59f9d574d082d88eb86300c1e
arshad008 <arshadrauff008@gmail.com>
2019-02-07 16:21:29 +0530

    finished todos list

---
+++ src/components/FinishedTodoList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    Row
+} from 'antd';
+import FinishedTodoListItem from './FinishedTodoListItem';
+
+const FinishedTodoList = (props) => {
+    let finishedTodos = props.finishedTodos.map((ftd, i)=>{
+        return <FinishedTodoListItem
+                    key={i}
+                    finishedTodo = {ftd}
+                    index={i}/>
+    });
+    return(
+        <Row gutter={16}>
+            {finishedTodos}
+        </Row>
+    );
+}
+
+export default FinishedTodoList;
