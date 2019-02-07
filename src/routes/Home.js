801d610dc733003416e6a3209aa16e6ab77ff643
arshad008 <arshadrauff008@gmail.com>
2019-02-07 15:17:05 +0530

    get todos data

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
+import { APIBaseUrl } from '../appConfig';
 
 class Home extends Component{
+    state = {
+        todos:[]
+    }
+    componentWillMount(){
+        let url = APIBaseUrl + "/todos";
+        Axios.get(url)
+        .then(res=>{
+            let data = res.data.result;
+            this.setState({todos: data});
+            console.log(this.state.todos);
+        }).catch(err=>console.error(err));
+    }
     render(){
         return(
             <div className="content">
-                hi
+                
             </div>
-        );
+        );                    
     }
 }
+
 export default Home;
